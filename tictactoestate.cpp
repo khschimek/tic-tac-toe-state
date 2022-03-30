3dc6d7caa853280e183c0018acc8ba7b92f7cffe
khschimek <78243133+khschimek@users.noreply.github.com>
2022-03-30 01:25:03 -0600

    Update tictactoestate.cpp

--- tictactoestate.cpp
+++ tictactoestate.cpp
@@ -31,10 +31,69 @@ int main() {
                 gameline += "111";
             }
         }
-        cout << gameline << endl;
         string playedSpots = gameline.substr(0, 9);
-        cout << playedSpots << endl;
-        string choiceSpots = gameline.substr(9, gameline.length());
-        cout << choiceSpots << endl;
+        string choiceSpots = gameline.substr(9, 18);
+        string tictac = "";
+        int j = 0;
+        for(int i=0; i<9; i++) {
+            if(playedSpots[i] == '1') {
+                if(choiceSpots[j] == '1') {
+                    tictac += "x";
+                }
+                else {
+                    tictac += "o";
+                }
+                j+=1;
+            }
+            else {
+                tictac += " ";
+            }
+        }
+        string topLine = tictac.substr(0, 3);
+        string oldMiddleLine = tictac.substr(3, 6);
+        string middleLine = oldMiddleLine.substr(0, 3);
+        string bottomLine = tictac.substr(6, 9);
+        /*
+        cout << topLine[0] << topLine[1] << topLine[2] << endl;
+        cout << middleLine[0] << middleLine[1] << middleLine[2] << endl;
+        cout << bottomLine[0] << bottomLine[1] << bottomLine[2] << endl;
+        */
+        if(topLine[0] == topLine[1] and topLine[1] == topLine[2] and topLine[2] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[0] == topLine[1] and topLine[1] == topLine[2] and topLine[2] == 'o') {
+            cout << "O wins" << endl;
+        } else if(middleLine[0] == middleLine[1] and middleLine[1] == middleLine[2] and middleLine[2] == 'x') {
+            cout << "X wins" << endl;
+        } else if(middleLine[0] == middleLine[1] and middleLine[1] == middleLine[2] and middleLine[2] == 'o') {
+            cout << "O wins" << endl;
+        } else if(bottomLine[0] == bottomLine[1] and bottomLine[1] == bottomLine[2] and bottomLine[2] == 'x') {
+            cout << "X wins" << endl;
+        } else if(bottomLine[0] == bottomLine[1] and bottomLine[1] == bottomLine[2] and bottomLine[2] == 'o') {
+            cout << "O wins" << endl;
+        } else if(topLine[0] == middleLine[0] and middleLine[0] == bottomLine[0] and bottomLine[0] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[0] == middleLine[0] and middleLine[0] == bottomLine[0] and bottomLine[0] == 'o') {
+            cout << "O wins" << endl;
+        } else if(topLine[1] == middleLine[1] and middleLine[1] == bottomLine[1] and bottomLine[1] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[1] == middleLine[1] and middleLine[1] == bottomLine[1] and bottomLine[1] == 'o') {
+            cout << "O wins" << endl;
+        } else if(topLine[2] == middleLine[2] and middleLine[2] == bottomLine[2] and bottomLine[2] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[2] == middleLine[2] and middleLine[2] == bottomLine[2] and bottomLine[2] == 'o') {
+            cout << "O wins" << endl;
+        } else if(topLine[0] == middleLine[1] and middleLine[1] == bottomLine[2] and bottomLine[2] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[0] == middleLine[1] and middleLine[1] == bottomLine[2] and bottomLine[2] == 'o') {
+            cout << "O wins" << endl;
+        } else if(topLine[2] == middleLine[1] and middleLine[1] == bottomLine[0] and bottomLine[0] == 'x') {
+            cout << "X wins" << endl;
+        } else if(topLine[2] == middleLine[1] and middleLine[1] == bottomLine[0] and bottomLine[0] == 'o') {
+            cout << "O wins" << endl;
+        } else if(playedSpots == "111111111") {
+            cout << "Cat's" << endl;
+        } else {
+            cout << "In progress" << endl;
+        }
     }
 }
